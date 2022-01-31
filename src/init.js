3461cc4e30a4fc82f2787ca1f6f929dcfd1fc13e
jnunez1229 <thatjuanguy89@gmail.com>
2022-01-30 18:15:29 -0600

    created handle events for buttons

--- src/init.js
+++ src/init.js
@@ -1,10 +1,11 @@
-import game from "./gameState";
+/* eslint-disable no-console */
+import game, {handleUserAction} from "./gameState";
 import { TICK_RATE } from "./constants";
 import initButtons from "./buttons";
 
 function init() {
   console.log("starting game");
-  initButtons(game.handleUserAction);
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
 
